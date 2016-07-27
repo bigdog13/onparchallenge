53cf72cd0732828f1890fa0bab5d8287217f6f88
bigdog13 <you@example.com>
2016-07-27 19:48:34 -0400

    first commit

---
+++ onparchallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onparchallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //ask user for 1 number
+            string playAgain = null;
+            do
+            {
+                Console.WriteLine("Pick a number");
+                int num = int.Parse(Console.ReadLine());
+                //write number and sum
+                Console.WriteLine("sum {0}", num);
+
+                //do you want another number
+                Console.WriteLine("Do you want another number?" );
+    
+
+               string playAgain = Console.ReadLine().ToUpper();
+                Console.Clear(); ;
+
+                if playAgain = no
+                {
+                    Console.WriteLine("GoodBye')
+                }
+
+
+
+                while (playAgain == "YES") ;
+            }
+
+
+
+
+        }
+    }
+}
